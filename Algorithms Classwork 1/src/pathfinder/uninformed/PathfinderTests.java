d5a95ab0ab93d0faa5d8f96ee19ee5df00246171
JigarSwam <jswaminarayan@gmail.com>
2019-01-22 19:13:41 -0800

    Added Tests

--- Algorithms Classwork 1/src/pathfinder/uninformed/PathfinderTests.java
+++ Algorithms Classwork 1/src/pathfinder/uninformed/PathfinderTests.java
@@ -46,5 +46,58 @@ public class PathfinderTests {
         assertEquals(1,  result[0]); // Test that result is a solution
         assertEquals(12, result[1]); // Ensure that the solution is optimal
     }
+    @Test
+    public void testPathfinder_t2() {
+        String[] maze = {
+            "XXXXXXXX",
+            "X.....XX",
+            "XIX.X.XX",
+            "XX.X..XX",
+            "X....GXX",
+            "XXXXXXXX"
+        };
+        MazeProblem prob = new MazeProblem(maze);
+        ArrayList<String> solution = Pathfinder.solve(prob);
+        
+        int[] result = prob.testSolution(solution);
+        assertEquals(1,  result[0]); // Test that result is a solution
+        assertEquals(8, result[1]); // Ensure that the solution is optimal
+    }
+    
+    @Test
+    public void testPathfinder_t3() {
+        String[] maze = {
+            "XXXXXXX",
+            "X.....X",
+            "XIX.X.X",
+            "XX.X..X",
+            "XX.G..X",
+            "XXXXXXX"
+        };
+        MazeProblem prob = new MazeProblem(maze);
+        ArrayList<String> solution = Pathfinder.solve(prob);
+        
+        int[] result = prob.testSolution(solution);
+        assertEquals(1,  result[0]); // Test that result is a solution
+        assertEquals(10, result[1]); // Ensure that the solution is optimal
+    }
+    
+    @Test
+    public void testPathfinder_t4() {
+        String[] maze = {
+            "XXXXXXXXXXXX",
+            "X......X...X",
+            "XIX.XX.....X",
+            "XXGXXXXXX..X",
+            "X..........X",
+            "XXXXXXXXXXXX"
+        };
+        MazeProblem prob = new MazeProblem(maze);
+        ArrayList<String> solution = Pathfinder.solve(prob);
+        
+        int[] result = prob.testSolution(solution);
+        assertEquals(1,  result[0]); // Test that result is a solution
+        assertEquals(20, result[1]); // Ensure that the solution is optimal
+    }
 
 }
