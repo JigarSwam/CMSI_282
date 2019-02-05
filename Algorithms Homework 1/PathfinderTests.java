17a9ea4af2744ffa2a7cb44e74a29ef100d043c3
JigarSwam <jswaminarayan@gmail.com>
2019-02-04 19:45:57 -0800

    Updated Files

--- Algorithms Homework 1/PathfinderTests.java
+++ Algorithms Homework 1/PathfinderTests.java
@@ -13,91 +13,70 @@ public class PathfinderTests {
     @Test
     public void testPathfinder_t0() {
         String[] maze = {
-            "XXXX",
-            "X.IX",
-            "XG.X",
-            "XXXX"
+            "XXXXXXX",
+            "XI...KX",
+            "X.....X",
+            "X.X.XGX",
+            "XXXXXXX"
         };
         MazeProblem prob = new MazeProblem(maze);
         ArrayList<String> solution = Pathfinder.solve(prob);
-        
+
         // result will be a 2-tuple (isSolution, cost) where
         // - isSolution = 0 if it is not, 1 if it is
         // - cost = numerical cost of proposed solution
         int[] result = prob.testSolution(solution);
         assertEquals(1, result[0]); // Test that result is a solution
-        assertEquals(2, result[1]); // Ensure that the solution is optimal
+        assertEquals(6, result[1]); // Ensure that the solution is optimal
     }
-    
+
     @Test
     public void testPathfinder_t1() {
         String[] maze = {
             "XXXXXXX",
-            "X.....X",
-            "XIX.X.X",
-            "XX.X..X",
-            "XG....X",
+            "XI....X",
+            "X.MMM.X",
+            "X.XKXGX",
             "XXXXXXX"
         };
         MazeProblem prob = new MazeProblem(maze);
         ArrayList<String> solution = Pathfinder.solve(prob);
-        
+
         int[] result = prob.testSolution(solution);
-        assertEquals(1,  result[0]); // Test that result is a solution
-        assertEquals(12, result[1]); // Ensure that the solution is optimal
+        assertEquals(1, result[0]);  // Test that result is a solution
+        assertEquals(14, result[1]); // Ensure that the solution is optimal
     }
+
     @Test
     public void testPathfinder_t2() {
         String[] maze = {
-            "XXXXXXXX",
-            "X.....XX",
-            "XIX.X.XX",
-            "XX.X..XX",
-            "X....GXX",
-            "XXXXXXXX"
+            "XXXXXXX",
+            "XI.G..X",
+            "X.MMMGX",
+            "X.XKX.X",
+            "XXXXXXX"
         };
         MazeProblem prob = new MazeProblem(maze);
         ArrayList<String> solution = Pathfinder.solve(prob);
-        
+
         int[] result = prob.testSolution(solution);
-        assertEquals(1,  result[0]); // Test that result is a solution
-        assertEquals(8, result[1]); // Ensure that the solution is optimal
+        assertEquals(1, result[0]);  // Test that result is a solution
+        assertEquals(10, result[1]); // Ensure that the solution is optimal
     }
-    
+
     @Test
     public void testPathfinder_t3() {
         String[] maze = {
             "XXXXXXX",
-            "X.....X",
-            "XIX.X.X",
-            "XX.X..X",
-            "XX.G..X",
+            "XI.G..X",
+            "X.MXMGX",
+            "X.XKX.X",
             "XXXXXXX"
         };
         MazeProblem prob = new MazeProblem(maze);
         ArrayList<String> solution = Pathfinder.solve(prob);
-        
-        int[] result = prob.testSolution(solution);
-        assertEquals(1,  result[0]); // Test that result is a solution
-        assertEquals(10, result[1]); // Ensure that the solution is optimal
-    }
-    
-    @Test
-    public void testPathfinder_t4() {
-        String[] maze = {
-            "XXXXXXXXXXXX",
-            "X......X...X",
-            "XIX.XX.....X",
-            "XXGXXXXXX..X",
-            "X..........X",
-            "XXXXXXXXXXXX"
-        };
-        MazeProblem prob = new MazeProblem(maze);
-        ArrayList<String> solution = Pathfinder.solve(prob);
-        
-        int[] result = prob.testSolution(solution);
-        assertEquals(1,  result[0]); // Test that result is a solution
-        assertEquals(20, result[1]); // Ensure that the solution is optimal
+
+        assertNull(solution); // Ensure that Pathfinder knows when there's no solution
     }
 
 }
